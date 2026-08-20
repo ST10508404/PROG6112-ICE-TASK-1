d6c0a7ed53d93fd9f3f8bd5c798605bf8041db33
ST10508404 <ST10508404@myemeris.edu.za>
2026-08-20 20:08:01 +0200

    Created project

---
+++ FlowerShop/src/main/java/com/mycompany/flowershop/FlowerShop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.flowershop;
+
+/**
+ *
+ * @author niena
+ */
+public class FlowerShop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
