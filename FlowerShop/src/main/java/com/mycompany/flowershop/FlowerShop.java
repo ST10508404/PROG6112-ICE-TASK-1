b4e97e46ab824e775a70ce5526e5a5a49d701042
ST10508404 <ST10508404@myemeris.edu.za>
2026-08-20 20:21:16 +0200

    Finished enterSales

--- FlowerShop/src/main/java/com/mycompany/flowershop/FlowerShop.java
+++ FlowerShop/src/main/java/com/mycompany/flowershop/FlowerShop.java
@@ -2,15 +2,15 @@
 package com.mycompany.flowershop;
 import java.util.*;
 public class FlowerShop {
-    String[][] salesEntries = new String[7][3];
-    Scanner input = new Scanner(System.in)
+    int[][] salesEntries = new int[7][3];
+    Scanner input = new Scanner(System.in);
     void enterSales()
     {
       for(int days = 0; days < salesEntries.length - 1; days++)
       {
           for(int flowers = 0; flowers < 2; flowers++)
           {
-              
+            salesEntries[days][flowers] = input.nextInt();
           }
       }  
     }
