73289ab8245179d65eb56145de1c3f32f46f206b
ST10508404 <ST10508404@myemeris.edu.za>
2026-08-20 20:34:03 +0200

    Finished displaySales

--- FlowerShop/src/main/java/com/mycompany/flowershop/FlowerShop.java
+++ FlowerShop/src/main/java/com/mycompany/flowershop/FlowerShop.java
@@ -21,10 +21,71 @@ public class FlowerShop
       System.out.println("=============================================");
       System.out.println("\tFLOWER SHOP WEEKLY SALES REPORT");
       System.out.println("=============================================");
+      System.out.println("\tDAY          ROSES    TULIPS    LILIES    TOTAL");
+      System.out.println("------------------------------------------------");
+      for(int day = 0; day < 6;day++)
+      {
+         switch(day)
+         {
+             case 0: 
+             System.out.println("Monday");
+             for(int flower = 0; flower < 2; flower++)
+             {
+              System.out.print("\t" + salesEntries[day][flower]);   
+             }
+             break;
+             case 1: 
+             System.out.println("Monday");
+             for(int flower = 0; flower < 2; flower++)
+             {
+              System.out.print("\t" + salesEntries[day][flower]);   
+             }
+             break;
+             case 2: 
+             System.out.println("Monday");
+             for(int flower = 0; flower < 2; flower++)
+             {
+              System.out.print("\t" + salesEntries[day][flower]);   
+             }
+             break;
+             case 3: 
+             System.out.println("Monday");
+             for(int flower = 0; flower < 2; flower++)
+             {
+              System.out.print("\t" + salesEntries[day][flower]);   
+             }
+             break;
+             case 4: 
+             System.out.println("Monday");
+             for(int flower = 0; flower < 2; flower++)
+             {
+              System.out.print("\t" + salesEntries[day][flower]);   
+             }
+             break;
+             case 5: 
+             System.out.println("Monday");
+             for(int flower = 0; flower < 2; flower++)
+             {
+              System.out.print("\t" + salesEntries[day][flower]);   
+             }
+             break;
+             case 6: 
+             System.out.println("Monday");
+             for(int flower = 0; flower < 2; flower++)
+             {
+              System.out.print("\t" + salesEntries[day][flower]);   
+             }
+             break;
+             default:
+              System.out.println("Wrong amount of days");
+             break;
+         }
+         System.out.println("------------------------------------------------");
+      }
     }
     public static void main(String[] args)
     {
-       FlowerShop flowers = new FlowerShop();
-       flowers.displaySales();
+       
+       
     }
 }
